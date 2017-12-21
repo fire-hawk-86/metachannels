4b59211691e01286525b27a070147c5d313ce74e
fire-hawk-86 <fire-hawk@gmx.net>
2017-12-21 10:03:33 +0100

    update

--- resources/views/youtube/video.blade.php
+++ resources/views/youtube/video.blade.php
@@ -37,7 +37,7 @@
                 <div style="min-height: 78px; font-size: .8em; position: relative;">
                     <a href="{{ url('video/'.$video->id->videoId) }}">
                         <img style="position: absolute; top: -11px; left: 0; clip: rect(11px,120px,79px,0px);" src="{{$video->snippet->thumbnails->default->url}}">
-                        <div style="margin-left: 130px;">{{$video->snippet->title}}</div>
+                        <div style="margin-left: 130px; line-height: 1.6; max-height: 3.2em; overflow: hidden;" title="{{ $video->snippet->title }}">{{$video->snippet->title}}</div>
                     </a>
                     <div style="margin-left: 130px;">{{Carbon\Carbon::parse($video->snippet->publishedAt)->format('d. F Y')}}</div>
                     <div style="margin-left: 130px; margin-bottom: 10px">Channel: <a href="{{ url('https://www.youtube.com/channel/'.$video->snippet->channelId) }}">{{ $video->snippet->channelTitle }}</a></div>
