b2ca8123025f6762ee5f01cc1330255ec94cbe47
fire-hawk-86 <fire-hawk@gmx.net>
2018-05-08 19:09:28 +0200

    add migration 'add_foreign_key_to_metachannels_videos_channel_metachannel'

---
+++ database/migrations/2018_05_08_170513_add_foreign_key_to_metachannels_videos_channel_metachannel.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyToMetachannelsVideosChannelMetachannel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('metachannels', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('videos', function (Blueprint $table) {
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+        });
+
+        Schema::table('channel_metachannel', function (Blueprint $table) {
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+            $table->foreign('metachannel_id')->references('id')->on('metachannels')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('metachannels', function (Blueprint $table) {
+            //
+        });
+    }
+}
