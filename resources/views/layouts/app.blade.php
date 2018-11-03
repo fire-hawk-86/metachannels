87462f02b4ad07e6f0d679d251a503798f8dfa85
fire-hawk-86 <fire-hawk@gmx.net>
2018-11-03 10:24:57 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,6 +126,11 @@
             </div>
 
             @yield('content')
+            <div class="container">
+                <div id="disqus_thread" class="text-center" style="margin-top: 22px;">
+                    <a class="btn btn-primary" style="display: inline-block;" href="#" onclick="disqus();return false;">Show Comments</a>
+                </div>
+            </div>
         </div>
 
         <!-- Footer -->
@@ -159,6 +164,36 @@
     </div>
     <!-- JS -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript">
+        // Replace labnol with your disqus shortname
+        var disqus_shortname = "metachannels";
+
+        // Put the permalink of your web page / blog post
+        var disqus_url = "{{ Request::url() }}";
+
+        // Put the permalink of your web page / blog post
+        var disqus_identifier = "{{ Request::url() }}";
+
+        var disqus_loaded = false;
+
+        // This is the function that will load Disqus comments on demand
+        function disqus() {
+
+          if (!disqus_loaded)  {
+
+            // This is to ensure that Disqus widget is loaded only once
+            disqus_loaded = true;
+
+            var e = document.createElement("script");
+            e.type = "text/javascript";
+            e.async = true;
+            e.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+            (document.getElementsByTagName("head")[0] ||
+             document.getElementsByTagName("body")[0])
+            .appendChild(e);
+          }
+        }
+    </script>
     @yield('scripts')
 </body>
 </html>
